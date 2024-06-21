337b4d6ffdf7d09fe8e67d2f6a15e48f3fe8efb7
anysg <2801772839@qq.com>
2024-06-21 12:24:25 +0800

    完善5个管理页面

---
+++ src/components/MdViewer.vue
@@ -0,0 +1,26 @@
+<template>
+  <Viewer :value="value" :plugins="plugins" />
+</template>
+
+<script lang="ts" setup>
+import { Viewer } from '@bytemd/vue-next'
+import gfm from '@bytemd/plugin-gfm'
+import highlight from '@bytemd/plugin-highlight'
+/**
+ * 定义组件属性类型
+ */
+interface Props {
+  value: string
+}
+
+defineProps<Props>()
+
+const plugins = [gfm(), highlight()]
+/**
+ * 给组件指定初始值
+ */
+const props = withDefaults(defineProps<Props>(), {
+  value: () => '',
+})
+</script>
+<style lang="less" scoped></style>
---
+++ src/views/admin/AdminScoringResultPage.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "AdminScoringResultPage"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped lang="less">
+
+</style>
